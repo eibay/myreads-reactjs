abb092aff522da35e1b44ef4b778dda5d1b2334b
Elmer Ibayan <eibayan@gmail.com>
2017-09-06 20:09:07 +1000

    docs: modify readme

--- src/App.js
+++ src/App.js
@@ -12,14 +12,7 @@ class BooksApp extends Component {
     search: PropTypes.bool
   }
 
-
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
      books: [],
      listTitles: ["MyReads", "MySearch"],
      shelves: [{slug: "currentlyReading", title: "Currently Reading", search: false},
